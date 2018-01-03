8c631f02013f6fd7640c52485b5728dd29125931
dengyin <dengyin@itiaoling.com>
2018-01-03 15:38:22 +0800

    通过 Feign 的 fallback 属性来实现 ribbon 机制的 服务降级

--- feign-consumer/src/main/java/com/yifan/spring/cloud/feign/service/impl/HelloServiceFallback.java
+++ feign-consumer/src/main/java/com/yifan/spring/cloud/feign/service/impl/HelloServiceFallback.java
@@ -1,48 +1,53 @@
-package com.yifan.spring.cloud.feign.service;
+package com.yifan.spring.cloud.feign.service.impl;
 
 import com.yifan.spring.cloud.feign.dto.User;
-import com.yifan.spring.cloud.feign.service.impl.HelloServiceFallback;
-import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.*;
+import com.yifan.spring.cloud.feign.service.HelloService;
 
 /**
  * @version V1.0
- * @Title: HelloService
+ * @Title: HelloServiceFallback
  * @Package: com.yifan.spring.cloud.feign.service
- * @Description: 通过 @FeignClient 注解的 fallback 属性来指定对应的服务降级实现类。
+ * @Description: 对于 @FeignClient 接口服务的 服务降级实现
  * @author: dengyin
  * @date: 18-1-3
  */
-@FeignClient(name = "monkey", fallback = HelloServiceFallback.class)
-public interface HelloService {
-
-    @RequestMapping("/hello")
-    String hello();
-
+public class HelloServiceFallback implements HelloService {
+    @Override
+    public String hello() {
+        return "error";
+    }
 
     /**
      * 这里 一 定要注意，在定义各参数绑定时，@RequestParam、@RequestHeader 等可
      * 以指定参数名称的注解， 它们的 value 千万不能少。 在 SpringMVC 程序中， 这些注解会根
      * 据参数名来作为默认值，但是在Feign 中绑定参数必须通过 value 属性来指明具体的参数名，
      * 不然会抛出口legalStateException 异常， value 属性不能为空
+     *
      * @param name
      * @return
      */
-    @RequestMapping(value = "/hello1", method = RequestMethod.GET)
-    String hello(@RequestParam("name") String name);
+    @Override
+    public String hello(String name) {
+        return "error";
+    }
 
     /**
      * 这里 一 定要注意，在定义各参数绑定时，@RequestParam、@RequestHeader 等可
      * 以指定参数名称的注解， 它们的 value 千万不能少。 在 SpringMVC 程序中， 这些注解会根
      * 据参数名来作为默认值，但是在Feign 中绑定参数必须通过 value 属性来指明具体的参数名，
      * 不然会抛出口legalStateException 异常， value 属性不能为空
+     *
      * @param name
      * @param age
      * @return
      */
-    @RequestMapping(value = "/hello2", method = RequestMethod.GET)
-    User hello(@RequestHeader("name") String name, @RequestHeader("age") Integer age);
+    @Override
+    public User hello(String name, Integer age) {
+        return new User("error",0);
+    }
 
-    @RequestMapping(value = "/hello3", method = RequestMethod.POST)
-    String hello(@RequestBody User user);
+    @Override
+    public String hello(User user) {
+        return "error";
+    }
 }
