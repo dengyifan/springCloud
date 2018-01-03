217369540099ac6f0e4cbfa8724471b7e5f063d9
dengyin <dengyin@itiaoling.com>
2018-01-03 16:37:52 +0800

    为 feign 添加全局日志级别配置

--- feign-consumer/src/main/java/com/yifan/spring/cloud/feign/FeignConsumerApplication.java
+++ feign-consumer/src/main/java/com/yifan/spring/cloud/feign/FeignConsumerApplication.java
@@ -1,14 +1,25 @@
 package com.yifan.spring.cloud.feign;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @EnableFeignClients //开启 Spring Cloud Feign 的支持功能
 @EnableDiscoveryClient
 @SpringBootApplication
 public class FeignConsumerApplication {
+
+	/**
+	 * 针对全局的日志级别配置
+	 * @return
+	 */
+	@Bean
+	Logger.Level feignLoggerLevel() {
+		return Logger.Level.FULL;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FeignConsumerApplication.class, args);
