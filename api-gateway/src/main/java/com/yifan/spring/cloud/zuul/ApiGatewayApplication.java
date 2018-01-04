7f02c1fbb8f06f5a5ad460869990f35576d92403
dengyin <dengyin@itiaoling.com>
2018-01-04 09:45:36 +0800

    添加 zuul 实现请求路由功能

---
+++ api-gateway/src/main/java/com/yifan/spring/cloud/zuul/ApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.yifan.spring.cloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy //开启 Zuul 的 API网关服务功能
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}
