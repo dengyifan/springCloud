99fa4bdc82e0619155ce63bef4aa0fc27c052e5c
dengyin <dengyin@itiaoling.com>
2018-01-12 15:00:44 +0800

    添加 trace-two

---
+++ trace-two/src/main/java/com/yifan/spring/cloud/sleuth/TraceTwoApplication.java
@@ -0,0 +1,12 @@
+package com.yifan.spring.cloud.sleuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraceTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraceTwoApplication.class, args);
+	}
+}
